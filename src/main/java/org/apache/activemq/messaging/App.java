7f1ab81cf1614305d17eeb798dcc86c23b86b9b7
Renato Marroquin <renatoj.marroquin@gmail.com>
2015-08-08 23:00:34 -0700

    First commit

---
+++ src/main/java/org/apache/activemq/messaging/App.java
@@ -0,0 +1,12 @@
+package org.apache.activemq.messaging;
+
+/**
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "ActiveMQ starting." );
+    }
+}
